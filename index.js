434610ff6feac5f9786ee3d223e8d51938c9a3ac
KevinJair11 <kevinchuquitarco11@gmail.com>
2026-01-05 12:01:45 -0500

    Falla de incluir

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use("/autor", require("./routes/autor"));
 app.use("/genero",require("./routes/genero"));
 app.use("/libro", require("./routes/libro"));
-app.use("example", require("./routes/mensajes"))
+app.use("/example", require("./routes/mensajes"))
 
 app.listen(PORT, ()=>{
     console.log("Escuando desde el puerto "+PORT);
